4c6047cbfad38b4616ad7c497dab5ec5b41a4328
Ashwin Kumaar <ashwinkumaar10@gmail.com>
2021-09-30 14:08:31 +0800

    updated with a different dashboard method

--- src/components/Dashboard/ProductList.js
+++ src/components/Dashboard/ProductList.js
@@ -1,24 +1,50 @@
-import React, { Component } from 'react';
-import ProductCard from './ProductCard';
+import React from 'react';
+import ProductCard2 from './ProductCard';
 
 //components/Dashboard/ProductList.js
 
-class ProductList extends Component {
-    // creates a ProductCard obj to be rendered
-    renderCard = (current_item) => {
-        return <ProductCard product={current_item}></ProductCard>;
+// class ProductList extends React.Component {
+//     // creates a ProductCard obj to be rendered
+//     renderCard = (current_item) => {
+//         return <ProductCard product={current_item}></ProductCard>;
+//     }
+
+//     // Iterate through  product list and calls renderCard
+//     // creates the ProductTile object
+//     render() {
+//         let cards = [];
+//         for (let i = 0; i < this.props.products.length; i++) {
+//             const current_item = this.props.products[i];
+//             cards.push(this.renderTile(current_item));
+//         }
+//         return cards;
+//     }
+// }
+
+// export default ProductList;
+
+class ProductList2 extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        products: [],
+      }
     }
 
-    // Iterate through  product list and calls renderCard
-    // creates the ProductTile object
     render() {
-        let cards = [];
-        for (let i = 0; i < this.props.products.length; i++) {
-            const current_item = this.props.products[i];
-            cards.push(this.renderTile(current_item));
-        }
-        return cards;
+      return (
+        <div className="container main-content">
+          {/* {
+            this.state.products.map(product => {
+              return <ProductCard2 key={product.id} title={product.title} description={product.description} price={product.price.formatted_with_symbol} image={product.image}  />
+            })
+          } */}
+          <ProductCard2 />
+          <ProductCard2 />
+        </div>
+      );
     }
-}
-
-export default ProductList;
+  }
+  
+  export default ProductList2;
--- src/components/Dashboard/dashboard.js
+++ src/components/Dashboard/dashboard.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProductList from './ProductList';
+import ProductList2 from './ProductList';
 import productData from '../../backend/dataset/products.json'
 
 
-const products = productData;
+// const products = productData;
 
 
-const Dashboard = function() {
-    ReactDOM.render(<ProductList products={products}></ProductList>, document.getElementById('product-list'));
+// const Dashboard = function() {
+//     ReactDOM.render(<ProductList products={products}></ProductList>, document.getElementById('product-list'));
+// }
+
+// Dashboard();
+
+// export default Dashboard;
+
+
+function Dashboard2() {
+    return (
+        <div className="container">
+            <header className="header">
+            <h1>Products</h1>
+            </header>
+            <ProductList2 />
+        </div>
+    );
 }
 
-export default Dashboard;
+export default Dashboard2;
