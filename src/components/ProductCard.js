4e7ddb23925970bbd45420bfde0340fef7f6ad18
Ashwin Kumaar <ashwinkumaar10@gmail.com>
2021-09-30 12:01:08 +0800

    product card 2nd update

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -9,52 +9,52 @@ import colors from "../config/colors";
 
 
 function Card({ title, price, description, imageUrl, qty }) {
-  return (
-    <View style={styles.card}>
-    <Image
-        style={styles.image}
-        tint="light"
-        uri={imageUrl}
-    />
-    <View style={styles.detailsContainer}>
-        <AppText style={styles.title} numberOflines={1}>
-        {title}
-        </AppText>
-        <AppText style={styles.price} numberOflines={1}>
-        {price}
-        </AppText>
-        <AppText style={styles.description} numberOflines={2}>
-        {description}
-        </AppText>
-        <AppText style={styles.qty} numberOflines={1}>
-        {qty}
-        </AppText>
-    </View>
-    </View>
-  );
+    return (
+        <View style={styles.card}>
+        <Image
+            style={styles.image}
+            tint="light"
+            uri={imageUrl}
+        />
+        <View style={styles.detailsContainer}>
+            <AppText style={styles.title} numberOflines={1}>
+            {title}
+            </AppText>
+            <AppText numberOflines={1}>
+            {price}
+            </AppText>
+            <AppText style={styles.description} numberOflines={2}>
+            {description}
+            </AppText>
+            <AppText numberOflines={1}>
+            {qty}
+            </AppText>
+        </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  card: {
-    borderRadius: 15,
-    backgroundColor: colors.white,
-    marginBottom: 20,
-    overflow: "hidden",
-  },
-  detailsContainer: {
-    padding: 20,
-  },
-  image: {
-    width: "100%",
-    height: 200,
-  },
-  description: {
-    color: colors.secondary,
-    fontWeight: "bold",
-  },
-  title: {
-    marginBottom: 7,
-  },
+    card: {
+      borderRadius: 15,
+      backgroundColor: colors.white,
+      marginBottom: 20,
+      overflow: "hidden",
+    },
+    detailsContainer: {
+      padding: 20,
+    },
+    image: {
+      width: "100%",
+      height: 200,
+    },
+    title: {
+      marginBottom: 7,
+    },
+    description: {
+        color: colors.secondary,
+        fontWeight: "bold",
+    },
 });
 
 export default Card;
