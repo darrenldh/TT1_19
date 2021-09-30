576d083d5d45612f76098616990f3f4f86694e21
darrenldh <darren909@gmail.com>
2021-09-30 11:49:15 +0800

    setting up token login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './login.css';
+import PropTypes from 'prop-types';
 
-export default function Login() {
-    return(
+async function loginUser(credentials) {
+    // return fetch('http://localhost:8080/login', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify(credentials)
+    // })
+    //     .then(data => data.json())
+    return {"token":"123"}
+}
+
+export default function Login({setToken}) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        // e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
+    return (
         <div className="login-wrapper">
             <h1>Welcome! Please Sign In</h1>
-            <form>
+            <form onSubmit={handleSubmit()}>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input type="text" onChange={e => setUserName(e.target.value)}/>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={e => setPassword(e.target.value)}/>
                 </label>
                 <div>
-                    <button type="submit">Submit</button>
+                    <button type="submit" >Submit</button>
                 </div>
             </form>
         </div>
     )
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
