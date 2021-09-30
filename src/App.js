890baf957a185d954e5cd51324c5b53665962db7
darrenldh <darren909@gmail.com>
2021-09-30 09:56:54 +0800

    added code scaffolding for login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from "./components/Dashboard/dashboard";
+import Preferences from "./components/Preferences/Preferences";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from "./components/Login/Login";
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [token, setToken] = useState();
+    if(!token) {
+        return <Login setToken={setToken} />
+    }
+
+    return (
+        <div className="wrapper">
+            <h1>Application</h1>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/preferences">
+                        <Preferences />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
