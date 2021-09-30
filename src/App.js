8c432164b4797077f54fd6b49fbb099e379fb2a2
darrenldh <darren909@gmail.com>
2021-09-30 11:49:15 +0800

    setting up token login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from "./components/Dashboard/dashboard";
 import Preferences from "./components/Preferences/Preferences";
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Login from "./components/Login/Login";
-import React, { useState } from 'react';
+import React from 'react';
+
+
+function setToken(userToken) {
+    sessionStorage.setItem('token', JSON.stringify(userToken));
+}
+
+function getToken() {
+    const tokenString = sessionStorage.getItem('token');
+    const userToken = JSON.parse(tokenString);
+    return userToken?.token
+}
+
 
 function App() {
-    const [token, setToken] = useState();
-    if(!token) {
-        return <Login setToken={setToken} />
-    }
+    const token = getToken();
 
+
+    if (!token) {
+        return <Login setToken={setToken}/>
+    }
     return (
         <div className="wrapper">
             <h1>Application</h1>
             <BrowserRouter>
                 <Switch>
                     <Route path="/dashboard">
-                        <Dashboard />
+                        <Dashboard/>
                     </Route>
                     <Route path="/preferences">
-                        <Preferences />
+                        <Preferences/>
                     </Route>
                 </Switch>
             </BrowserRouter>
