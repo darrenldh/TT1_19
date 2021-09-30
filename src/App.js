9af115b9192e368c2bb40c86404cd6dfa6fd6878
Wei Heng <ge28hes@mytum.de>
2021-09-30 11:31:04 +0800

    added data to public and server,js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,32 @@
-import './App.css';
-import Dashboard from "./components/Dashboard/dashboard";
-import Preferences from "./components/Preferences/Preferences";
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import Login from "./components/Login/Login";
-import React from 'react';
-
-
-function setToken(userToken) {
-    sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-    const tokenString = sessionStorage.getItem('token');
-    const userToken = JSON.parse(tokenString);
-    return userToken?.token
-}
-
+import logo from './logo.svg'
+import './App.css'
+import Dashboard from './components/Dashboard/dashboard'
+import Preferences from './components/Preferences/Preferences'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Login from './components/Login/Login'
+import React, { useState } from 'react'
 
 function App() {
-    const token = getToken();
+  const [token, setToken] = useState()
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
 
-
-    if (!token) {
-        return <Login setToken={setToken}/>
-    }
-    return (
-        <div className="wrapper">
-            <h1>Application</h1>
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/dashboard">
-                        <Dashboard/>
-                    </Route>
-                    <Route path="/preferences">
-                        <Preferences/>
-                    </Route>
-                </Switch>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div className='wrapper'>
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
+          <Route path='/preferences'>
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  )
 }
 
-export default App;
+export default App
